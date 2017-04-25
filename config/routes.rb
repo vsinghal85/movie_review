0033c8a91b5b5dd06bcaa4507485e6d554366a8f
Vaibhav Singhal <vsinghal85@gmail.com>
2017-04-25 08:35:37 -0400

    Movie is related to reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :reviews
+  resources :reviews, except: [:show,:index]
   devise_for :users
-  resources :movies
+  resources :movies do
+    resources :reviews, except: [:show,:index]
+  end
    root to: "movies#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
